a7660704ddf0a69c8c2b9f9d5691b3ba8e0897a8
Ben Shani <benshani04@gmail.com>
2022-06-19 17:13:18 +0300

    added timed motions based on measured cycle time

---
+++ mpu6050_timed/state_history.h
@@ -0,0 +1,51 @@
+#ifndef STATE_HISTORY_H
+#define STATE_HISTORY_H
+
+class StateHistory {
+    public:
+        int buff_size;
+        int i;
+        int* state_buff;
+        bool was_buff_filled;
+        StateHistory(int buff_size) : buff_size(buff_size), i(0), was_buff_filled(false) {
+            state_buff = new int[buff_size];
+        }
+
+        ~StateHistory() {
+            delete[] state_buff;
+        }
+
+        void update_state(int new_state) {
+            state_buff[i] = new_state;
+            if (!was_buff_filled && i == buff_size - 1) {
+                was_buff_filled = true;
+            }
+            i = (i + 1) % buff_size;
+        }
+
+        bool compare_lower_half(int state) {
+            if (!was_buff_filled) return false;
+
+            int base_i = (i + 1) % buff_size;
+            return compare_internal(base_i, state);
+        }
+
+        bool compare_higher_half(int state) {
+            if (!was_buff_filled) return false;
+            
+            int base_i = (i + 1 - (buff_size / 2)) % buff_size;
+            return compare_internal(base_i, state);
+        }
+
+        bool compare_internal(int base_i, int state) {
+            for (int j = base_i; j < base_i + (buff_size / 2); j++) {
+                if (state_buff[j] != state) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+};
+
+#endif
